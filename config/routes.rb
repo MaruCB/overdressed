684e537c1dce97bcb4d36d086b7e65fb1c757588
filipe bernardo <filipebernardo.fb@gmail.com>
2022-05-30 16:00:18 +0100

    user devise gems and booking controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  root to: "users#home"
+  resources :costumes, only: [:index, :show, :new, :destroy] do
+    resources :bookings, only: :new
+  end
+  get "my_bookings", to: "bookings#my_bookings"
 end
