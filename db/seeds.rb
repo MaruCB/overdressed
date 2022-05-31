4b7f4a7fd43f1f5766d8e8b6e0e0d69f4f9704d8
Maria Casado <casado_me@hotmail.com>
2022-05-31 17:27:50 +0100

    create customer to be able to test heroku. Pushing to start with latest master version

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Costume.destroy_all
 
-Costume.create(name: "Bird", price: "30", description: "It's another bird", size: "L", category: "Animals", photo_url: "https://m.media-amazon.com/images/I/61OABgGLfuL._AC_UX385_.jpg")
+Costume.create!(name: "Bird", price: "30", description: "It's another bird", size: "L", category: "Animals", photo_url: "https://m.media-amazon.com/images/I/61OABgGLfuL._AC_UX385_.jpg")
