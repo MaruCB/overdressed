60204d1bb1b2e5deb069aa0ba583254426caf12a
m6rie <90363284+m6rie@users.noreply.github.com>
2022-06-01 13:07:28 +0100

    Update bookings_controller.rb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  before_action :set_costume, only: [new, :create]
+  before_action :set_costume, only: [:new, :create]
   def new
     @booking = Booking.new
   end
