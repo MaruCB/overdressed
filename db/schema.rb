19d6b11d7385d935006692bed521dc68ee16f355
Maria Casado <casado_me@hotmail.com>
2022-06-01 11:21:51 +0100

    pushing to start with the same main as my team mates

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_31_112028) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
