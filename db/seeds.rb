5b76b85c0a1911d13a9e813acaeb43056b058eb7
filipe bernardo <filipebernardo.fb@gmail.com>
2022-06-03 15:33:25 +0100

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,20 +21,20 @@ User.create(first_name: "Marie", username: "Marie", email: "marie@gmail.com", pa
 User.create(first_name: "Tom", username: "Tom", email: "tom@gmail.com", password: "password")
 User.create(first_name: "Hector", username: "Hector", email: "hector@gmail.com", password: "password")
 User.create(first_name: "Leanna", username: "Leanna", email: "leanna@gmail.com", password: "password")
+User.create(first_name: "Paul", username: "paul", email: "paul@gmail.com", password: "password")
 
 puts "created #{User.count} users"
 puts "creating costumes..."
 
-Costume.create(name: "Car", price: "30", description: "Vroum vroum", size: "L", category: "Folk", photo_url: "https://i.ebayimg.com/images/g/Lc8AAOSwLxZiQ07O/s-l300.jpg", user_id:1)
-Costume.create(name: "Lama", price: "30", description: "It's a lama", size: "L", category: "Animals", photo_url: "https://images.fun.co.uk/products/41458/2-1-106511/llama-adult-costume.jpg", user_id:1)
-Costume.create(name: "Vampire", price: "30", description: "Edward Cullen's cousin", size: "L", category: "Folk", photo_url: "https://cdn.shopify.com/s/files/1/0023/6178/6426/products/fever-male-vampire-costume.jpg?v=1603019908", user_id:1)
-Costume.create(name: "Santa", price: "30", description: "It's another bird", size: "L", category: "Chritmas", photo_url: "https://www.fancydresstown.co.uk/wp-content/uploads/2019/04/2d86078e-1285-4789-b496-b5d77e41ef46_1.f03bd87635645ca7b09aa5571e6379db.jpeg", user_id:1)
-Costume.create(name: "Avatar", price: "30", description: "James Cameron's son", size: "L", category: "Fantasy", photo_url: "https://cdn.shopify.com/s/files/1/0276/9308/8803/products/Deluxe_Jake_Sully_Costume_1200x1200.jpg?v=1575226311", user_id:1)
-Costume.create(name: "Unicorn", price: "30", description: "It's another bird", size: "L", category: "Animals", photo_url: "https://canary.contestimg.wish.com/api/webimage/5f3e4c9815a731fab60aceed-large.jpg?cache_buster=68ea9fd65b3a013941ba6e8720453774", user_id:1)
-Costume.create(name: "Gatsby", price: "30", description: "Back to 1922", size: "L", category: "Fancy", photo_url: "https://cdn.shopify.com/s/files/1/0023/6178/6426/products/flapper-costume-black-with-long-dress_900x.jpg?v=1603010553", user_id:1)
-Costume.create(name: "Fairy", price: "30", description: "Tinker Bell's property. Please return", size: "L", category: "Folk", photo_url: "https://images.halloweencostumes.co.uk/products/63738/1-1/mens-forest-fairy-costume.jpg", user_id:1)
-Costume.create(name: "Mother Nature", price: "30", description: "Because our planet matters", size: "L", category: "Fantasy", photo_url: "https://www.seekpng.com/png/detail/415-4159040_mother-nature-mother-nature-costume.png", user_id:1)
-Costume.create(name: "Tomato", price: "30", description: "Your daily portion of fruits", size: "L", category: "Party", photo_url: "https://images.halloweencostumes.co.uk/products/67323/1-1/adult-inflatable-tomato-costume.jpg", user_id:1)
+Costume.create(name: "Car", price: "45", description: "Vroum vroum", size: "L", category: "Folk", photo_url: "https://i.ebayimg.com/images/g/Lc8AAOSwLxZiQ07O/s-l300.jpg", user_id:1)
+Costume.create(name: "Lama", price: "29", description: "It's a lama", size: "L", category: "Animals", photo_url: "https://images.fun.co.uk/products/41458/2-1-106511/llama-adult-costume.jpg", user_id:1)
+Costume.create(name: "Vampire", price: "75", description: "Edward Cullen's cousin", size: "L", category: "Folk", photo_url: "https://cdn.shopify.com/s/files/1/0023/6178/6426/products/fever-male-vampire-costume.jpg?v=1603019908", user_id:1)
+Costume.create(name: "Santa", price: "58", description: "It's another bird", size: "L", category: "Chritmas", photo_url: "https://www.fancydresstown.co.uk/wp-content/uploads/2019/04/2d86078e-1285-4789-b496-b5d77e41ef46_1.f03bd87635645ca7b09aa5571e6379db.jpeg", user_id:1)
+Costume.create(name: "Avatar", price: "90", description: "James Cameron's son", size: "L", category: "Fantasy", photo_url: "https://cdn.shopify.com/s/files/1/0276/9308/8803/products/Deluxe_Jake_Sully_Costume_1200x1200.jpg?v=1575226311", user_id:1)
+Costume.create(name: "Gatsby", price: "140", description: "Back to 1922", size: "L", category: "Fancy", photo_url: "https://cdn.shopify.com/s/files/1/0023/6178/6426/products/flapper-costume-black-with-long-dress_900x.jpg?v=1603010553", user_id:1)
+Costume.create(name: "Fairy", price: "48", description: "Tinker Bell's property. Please return", size: "L", category: "Folk", photo_url: "https://images.halloweencostumes.co.uk/products/63738/1-1/mens-forest-fairy-costume.jpg", user_id:1)
+Costume.create(name: "Mother Nature", price: "65", description: "Because our planet matters", size: "L", category: "Fantasy", photo_url: "https://www.seekpng.com/png/detail/415-4159040_mother-nature-mother-nature-costume.png", user_id:1)
+Costume.create(name: "Tomato", price: "29", description: "Your daily portion of fruits", size: "L", category: "Party", photo_url: "https://images.halloweencostumes.co.uk/products/67323/1-1/adult-inflatable-tomato-costume.jpg", user_id:1)
 
 puts "created #{Costume.count} costumes"
 puts "creating bookings..."
@@ -48,6 +48,5 @@ Booking.create(start_date: "21/07/2022", end_date: "30/07/2022", user_id:5, cost
 Booking.create(start_date: "03/09/2022", end_date: "07/09/2022", user_id:6, costume_id: 7)
 Booking.create(start_date: "15/10/2022", end_date: "28/10/2022", user_id:7, costume_id: 8)
 Booking.create(start_date: "29/08/2022", end_date: "03/09/2022", user_id:8, costume_id: 9)
-Booking.create(start_date: "30/06/2022", end_date: "03/07/2022", user_id:9, costume_id: 10)
 
 puts "created #{Booking.count} bookings"
